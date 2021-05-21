8ad0ac6b1fc9b9a30c6d0ab3fed80d2f2ce4af5a
Bill <bill@atomix.com.au>
2021-05-21 16:32:36 +0930

    Fix/terminal improvements and api capable of handling cors

---
+++ terminal/src/components/fail.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../App.scss'
+
+interface FailProps {
+
+}
+
+export const Fail:React.FC<FailProps> = () => {
+
+  return (
+    <div>
+      <h3>Fail</h3>
+    </div>
+  );
+}
+
+export default Fail;
