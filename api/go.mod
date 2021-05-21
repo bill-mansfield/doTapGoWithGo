8d8823c6f242446bf5ac9ba8dc6ad12a13735458
Bill <bill@atomix.com.au>
2021-05-21 16:34:20 +0930

    Fix/handle cors

--- api/go.mod
+++ api/go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/rs/cors v1.7.0
 	github.com/stripe/stripe-go v70.15.0+incompatible
 	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
--- api/main.go
+++ api/main.go
@@ -1,66 +1,92 @@
 package main
 
 import (
-		"log"
-		"net/http"
-		"os"
+	"log"
+	"net/http"
+	"os"
 
-		"github.com/gin-gonic/gin"
-		"github.com/joho/godotenv"
-		"github.com/stripe/stripe-go"
-		"github.com/stripe/stripe-go/charge"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/charge"
 )
 
 type ChargeJSON struct {
-		Amount				int64 	`json:"amount"`
-		ReceiptEmail 	string 	`json:"receiptEmail"`
+	Amount       int64  `json:"amount"`
+	ReceiptEmail string `json:"receiptEmail"`
 }
 
-func main() {		
-    err := godotenv.Load()
+func main() {
+	err := godotenv.Load()
 
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
-    
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-  // set up server
-  r := gin.Default()
+	// set up server
+	r := gin.Default()
 
-  // basic hello world GET route
-  r.GET("/", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-            "message": "Hello, World!",
-    })
-  })
+	// basic hello world GET route
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
 
-  // our basic charge API route
-  r.POST("/api/charges", func(c *gin.Context) {
-    // we will bind our JSON body to the `json` var
-    var json ChargeJSON
-    c.BindJSON(&json)
+	r.OPTIONS("/api/charges", preflight)
 
-    // Set Stripe API key
-    apiKey := os.Getenv("SK_TEST_KEY")
-    stripe.Key = apiKey
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		"atomix": "atomix",
+	}))
 
-    // Attempt to make the charge.
-    // We are setting the charge response to _
-    // as we are not using it.
-    _, err := charge.New(&stripe.ChargeParams{
-      Amount:       stripe.Int64(json.Amount),
-      Currency:     stripe.String(string(stripe.CurrencyAUD)),
-      Source:       &stripe.SourceParams{Token: stripe.String("tok_visa")}, // this should come from clientside
-      ReceiptEmail: stripe.String(json.ReceiptEmail)})
+	// our basic charge API route
+	authorized.POST("/api/charges", func(c *gin.Context) {
 
-    if err != nil {
-      // Handle any errors from attempt to charge
-      c.String(http.StatusBadRequest, "Request failed")
-      return
-    }
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Origin, Authorization, X-Requested-With")
 
-    c.String(http.StatusCreated, "Successfully charged")
-  })
+		// we will bind our JSON body to the `json` var
+		var json ChargeJSON
+		c.BindJSON(&json)
 
-  r.Run(":8080")
+		// Set Stripe API key
+		apiKey := os.Getenv("SK_TEST_KEY")
+		stripe.Key = apiKey
+
+		// Attempt to make the charge.
+		// We are setting the charge response to _
+		// as we are not using it.
+		chargeResponse, err := charge.New(&stripe.ChargeParams{
+			Amount:       stripe.Int64(json.Amount),
+			Currency:     stripe.String(string(stripe.CurrencyAUD)),
+			Source:       &stripe.SourceParams{Token: stripe.String("tok_visa")}, // this should come from clientside
+			ReceiptEmail: stripe.String(json.ReceiptEmail)})
+
+		if err != nil {
+			// Handle any errors from attempt to charge
+			c.JSON(http.StatusBadRequest, gin.H{
+				"code":           http.StatusBadRequest,
+				"message":        "Fail!",
+				"stripeResponse": chargeResponse,
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"code":           http.StatusOK,
+			"message":        "Success!",
+			"stripeResponse": chargeResponse,
+		})
+	})
+
+	r.Run(":8080")
+}
+
+func preflight(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+	c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers, Authorization, Content-Type")
+	c.Header("Access-Control-Allow-Credentials", "true")
+	c.JSON(http.StatusOK, struct{}{})
 }
